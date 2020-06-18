90c36bb9546752d17423ba3f4835ec184c927974
Vanessa Mauna <vmauna@aol.com>
2020-06-18 19:19:24 -0400

    Second commit

---
+++ components/navbar.js
@@ -0,0 +1,112 @@
+import Link from 'next/link'
+import Img from '../images/IMG_0102.jpeg'
+const Navbar = () => {
+  return (
+    <nav>
+      <style jsx>
+        {`
+        @import url('https://fonts.googleapis.com/css2?family=Lexend+Tera&family=Archivo+Black&family=Work+Sans&display=swap');
+        nav {
+            width: 100vw;
+            height: 50px;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            justify-content: center;
+            align-content: center;
+            animation: waves 8s infinite;
+          }
+
+          #name {
+            padding: .5em;
+            letter-spacing: 1px;
+            font-style: bold;
+            font-size: 30px;
+            font-family: 'Archivo Black', sans-serif;
+            text-transform: uppercase;
+            font-weight: 800;
+            opacity: 1;
+            animation: waves 8s ease-out 0s alternate infinite;
+          }
+          @keyframes waves{
+            0% {
+             color: #001F3F;
+            }
+            25%{
+              color: #eee;
+            }
+            50%{
+             color: orange;
+            }
+            100% {
+             color: #FF4136;
+            }
+          }
+
+          ul{
+            display: flex;
+            flex-direction:row;
+            margin: 0;
+            padding: 0;
+            justify-content: center;
+
+          }
+          li {
+            text-align: center;
+            letter-spacing: 3px;
+            font-weight: 800;
+            margin-right: 5em;
+            text-transform: uppercase;
+            list-style-type: none;
+          }
+          a {
+            text-decoration: none;
+            color:white;
+            font-size: 14px;  
+            font-family: 'work sans',sans-serif;
+          }
+
+          a:hover{
+            color: orange;
+          }
+         
+
+        `}
+      </style>
+  
+      <ul>
+      <li><Link href='/#watch'>
+        <a title='Watch'>
+          Watch
+        </a>
+      </Link>
+      </li>
+      <li>
+      <Link href='/#awards'>
+        <a title='Awards'>
+          Awards
+        </a>
+      </Link>
+      </li>
+      <li>
+      <Link href='/#crew'>
+        <a title='crew'>
+          Crew
+        </a>
+      </Link>
+      </li>
+      <li>
+        <a href='http://www.ignitemovie.com' title='Ignite Movie'>
+          Ignite
+        </a>
+      </li>
+      </ul>
+      
+      
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Layout from '../components/layout'
+import Home from './home'
+
+const Index = () => (
+  <Layout>
+    <div>
+      <Home />
+    </div>
+
+    <style jsx global>{`
+  html,
+  body {
+    padding: 0;
+    background: black;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    width: 100%;
+    margin-bottom: auto;
+    box-sizing: border-box;
+    flex-grow: 1;
+    flex-shrink: 1;
+  }
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  }
+`}</style>
+  </Layout>
+)
+
+export default Index
